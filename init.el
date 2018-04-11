76b289ea977d76a30670705e608e83f540c7140b
Peter-Chou <zhou_00009@126.com>
2018-04-11 15:45:05 +0800

    change emacs startup and emacsclient frame open as fullscreen

--- init.el
+++ init.el
@@ -178,7 +178,7 @@ values."
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
-                         gruvbox-dark-medium
+                         gruvbox-dark-hard
                          gruvbox-light-medium
                          ;; solarized-light
                          ;; dracula
@@ -281,14 +281,14 @@ values."
    dotspacemacs-loading-progress-bar t
    ;; If non nil the frame is fullscreen when Emacs starts up. (default nil)
    ;; (Emacs 24.4+ only)
-   dotspacemacs-fullscreen-at-startup nil
+   dotspacemacs-fullscreen-at-startup t
    ;; If non nil `spacemacs/toggle-fullscreen' will not use native fullscreen.
    ;; Use to disable fullscreen animations in OSX. (default nil)
    dotspacemacs-fullscreen-use-non-native nil
    ;; If non nil the frame is maximized when Emacs starts up.
    ;; Takes effect only if `dotspacemacs-fullscreen-at-startup' is nil.
    ;; (default nil) (Emacs 24.4+ only)
-   dotspacemacs-maximized-at-startup t
+   dotspacemacs-maximized-at-startup nil
    ;; A value from the range (0..100), in increasing opacity, which describes
    ;; the transparency level of a frame when it's active or selected.
    ;; Transparency can be toggled through `toggle-transparency'. (default 90)
@@ -405,12 +405,12 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
-  ;; (spacemacs/load-theme 'gruvbox-light-soft)
+  ;; make new frame fullscreen as default
+  (add-to-list 'default-frame-alist '(fullscreen . fullboth))
   ;; Use Spacemacs as the $EDITOR for git commits
   ;; (global-git-commit-mode t)
   ;; show time on powerline
   ;; copy from https://github.com/syl20bnr/spacemacs/issues/9458
-
   (setq display-time-24hr-format t)
   (setq display-time-format "%H:%M:%S")        ; add seconds
   (setq display-time-interval 1)               ; update every second
