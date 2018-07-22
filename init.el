5b43d1099a45214443d93f7415168d64f5fef7f4
Peter-Chou <zhou_00009@126.com>
2018-07-22 15:04:29 +0800

    add custom-set-variables

--- init.el
+++ init.el
@@ -1009,5 +1009,13 @@ This function is called at the very end of Spacemacs initialization."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((((class color) (min-colors 257)) nil) (((class color) (min-colors 89)) (:background "#1c1c1c" :foreground "#eeeeee"))))
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 4.0))))
+ '(font-lock-function-name-face ((t (:bold t))))
+ '(font-lock-keyword-face ((t (:bold t))))
+ '(font-lock-type-face ((t (:bold t))))
+ '(whitespace-indentation ((t (:background nil))))
+ '(whitespace-space ((t (:background nil))))
+ '(whitespace-tab ((t (:background nil :bold t))))
  )
 )
