be5c895c3e27c2095e9234d56ab657f2d7ab83e7
Peter-Chou <zhou_00009@126.com>
2018-12-13 16:03:06 +0800

    set shell buffer maximum size to 5000

--- lisp/init-msys2.el
+++ lisp/init-msys2.el
@@ -1,3 +1,4 @@
+(setq system-uses-terminfo nil)
 
 (when (eq system-type 'windows-nt)
   (setq explicit-shell-file-name "C:/msys64/usr/bin/zsh.exe")
@@ -5,8 +6,12 @@
   ;; (setq explicit-bash.exe-args '("--noediting" "--login" "-i"))
   (setq explicit-shell-args '("--login" "-i"))
   (setenv "SHELL" shell-file-name)
+  (setq comint-buffer-maximum-size 5000)
+  (setq comint-scroll-show-maximum-output t)
   (add-hook 'comint-output-filter-functions
             'comint-strip-ctrl-m)
+  (add-hook 'comint-output-filter-functions
+            'comint-truncate-buffer)
 
   (defun my-buffer-face-mode-variable ()
     "Set font to a variable width (proportional) fonts in current buffer"
