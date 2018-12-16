b53e5e0a56c5bcdbe6c97ec3653efa39b2cae8a8
Peter-Chou <zhou_00009@126.com>
2018-12-16 14:46:05 +0800

    set function name default color

--- init.el
+++ init.el
@@ -584,5 +584,6 @@ This function is called at the very end of Spacemacs initialization."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(font-lock-function-name-face ((t (:bold t :foreground "#F7C508"))))
  )
 )
