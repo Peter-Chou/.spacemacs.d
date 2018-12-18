e4883a25dd481a16f9cd0c97c793c0dbff5ca88b
Peter-Chou <zhou_00009@126.com>
2018-12-18 18:18:00 +0800

    use custom.el

--- init.el
+++ init.el
@@ -50,7 +50,7 @@ This function should only modify configuration layer settings."
      (python :variables
              python-backend 'anaconda
              python-test-runner 'pytest
-             python-fill-column 99
+             python-fill-column 80
              python-sort-imports-on-save t)
      (c-c++ :variables
             c-c++-default-mode-for-headers 'c++-mode
@@ -536,21 +536,20 @@ If you are unsure, try setting them in `dotspacemacs/user-config' first."
       ))
 
   (add-to-list 'load-path "~/.spacemacs.d/lisp/")
-  ;; utf-8-unix save file only has \n instead of (\r\n in windows)
 
-  ;; (add-to-list 'file-coding-system-alist '("\\.py" . utf-8-unix))
-  ;; (set-buffer-file-coding-system 'utf-8-unix)
+  (setq custom-file (expand-file-name "custom.el" dotspacemacs-directory))
 
   (setq theming-modifications
         '((darktooth
            ;; Font locking
-           (font-lock-function-name-face :bold t :foreground "#F7C508")
-           )
-          (leuven
-           (font-lock-function-name-face :bold t)
+           (font-lock-function-name-face :foreground "#F7C508")
            )
           )
         )
+
+  ;; utf-8-unix save file only has \n instead of (\r\n in windows)
+  ;; (add-to-list 'file-coding-system-alist '("\\.py" . utf-8-unix))
+  ;; (set-buffer-file-coding-system 'utf-8-unix)
 
   )
 
@@ -572,13 +571,15 @@ before packages are loaded."
   (require 'init-binding)
   (require 'init-fix)
   (require 'init-function)
-  (require 'init-default)
   (require 'init-msys2-shell)
   ;; (require 'init-cygwin-shell)
   (require 'init-misc)
+  (require 'init-default)
 
   ;; sever started by spacemacs function seems to fail from time to time
   (server-start)
+
+  (load-file custom-file)
 
   )
 
@@ -587,18 +588,4 @@ before packages are loaded."
 This is an auto-generated function, do not modify its content directly, use
 Emacs customize menu instead.
 This function is called at the very end of Spacemacs initialization."
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (yapfify pyvenv pytest pyenv-mode py-isort pippel pipenv pip-requirements live-py-mode importmagic epc ctable concurrent deferred cython-mode company-anaconda anaconda-mode pythonic yasnippet-snippets yaml-mode xterm-color ws-butler writeroom-mode winum which-key wgrep web-mode web-beautify volatile-highlights uuidgen use-package unfill treemacs-projectile treemacs-evil toc-org tagedit symon string-inflection sql-indent spaceline-all-the-icons smex smeargle slim-mode shell-pop scss-mode sass-mode restart-emacs request ranger rainbow-delimiters py-autopep8 pug-mode prettier-js popwin persp-mode pcre2el password-generator paradox overseer orgit org-projectile org-present org-pomodoro org-mime org-download org-bullets org-brain open-junk-file nameless mwim multi-term move-text mmm-mode markdown-toc magit-svn magit-gitflow macrostep lsp-ui lorem-ipsum link-hint ivy-yasnippet ivy-xref ivy-rtags ivy-purpose ivy-hydra insert-shebang indent-guide impatient-mode ibuffer-projectile hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation highlight-indent-guides helm-make google-translate google-c-style golden-ratio gnuplot gitignore-templates gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gh-md fuzzy font-lock+ flycheck-rtags flycheck-pos-tip flycheck-bashate flx-ido fish-mode fill-column-indicator fancy-battery eyebrowse expand-region evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-org evil-numbers evil-mc evil-matchit evil-magit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-goggles evil-exchange evil-escape evil-ediff evil-commentary evil-cleverparens evil-args evil-anzu eval-sexp-fu eshell-z eshell-prompt-extras esh-help emmet-mode elisp-slime-nav editorconfig dumb-jump dotenv-mode doom-modeline disaster diminish define-word default-text-scale darktooth-theme csv-mode cquery counsel-projectile counsel-css company-web company-statistics company-shell company-rtags company-quickhelp company-lsp company-c-headers column-enforce-mode cmake-mode cmake-ide clean-aindent-mode clang-format centered-cursor-mode ccls beacon auto-yasnippet auto-highlight-symbol auto-compile all-the-icons-ivy all-the-icons-dired aggressive-indent ace-link ac-ispell))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(font-lock-function-name-face ((t (:bold t :foreground "#F7C508")))))
 )
