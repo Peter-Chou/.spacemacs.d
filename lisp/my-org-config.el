1bc7fdd05cd0e88db686bfe26baccfe623029e80
Peter-Chou <zhou_00009@126.com>
2018-12-19 18:41:42 +0800

    update org config

--- lisp/my-org-config.el
+++ lisp/my-org-config.el
@@ -1,4 +1,13 @@
+(with-eval-after-load 'org
+  (setq org-startup-folded 'content)
+  )
 
-(setq org-startup-folded 'content)
+;; add project's TODO files to the agenda automatically
+(with-eval-after-load 'org-agenda
+  (require 'org-projectile)
+  (mapcar '(lambda (file)
+             (when (file-exists-p file)
+               (push file org-agenda-files)))
+          (org-projectile-todo-files)))
 
 (provide 'my-org-config)
