3951775bdf50a341e661580845c8934f38e91147
Peter-Chou <zhou_00009@126.com>
2018-12-29 20:51:20 +0800

    add treemacs-icons-dired

--- layers/peter-misc/packages.el
+++ layers/peter-misc/packages.el
@@ -15,15 +15,17 @@
   '(
     all-the-icons
     all-the-icons-ivy
-    all-the-icons-dired
     beacon
+    ;; company-box
     default-text-scale
     electric-operator
     evil-vimish-fold
     fontify-face
     highlight-indent-guides
     prettify-greek
-    smart-semicolon))
+    smart-semicolon
+    treemacs-icons-dired
+    ))
 
 (defun peter-misc/init-all-the-icons ()
   (use-package all-the-icons
@@ -37,16 +39,17 @@
     :config
     (all-the-icons-ivy-setup)))
 
-(defun peter-misc/init-all-the-icons-dired ()
-  (use-package all-the-icons-dired
-    :defer t
-    :hook ((dired-mode . all-the-icons-dired-mode))))
-
 (defun peter-misc/init-beacon ()
   (use-package beacon
     :ensure t
     :config
     (beacon-mode 1)))
+
+;; (defun peter-misc/init-company-box ()
+;;   (use-package company-box
+;;     :if (display-graphic-p)
+;;     :defer t
+;;     :hook (company-mode . company-box-mode)))
 
 (defun peter-misc/init-default-text-scale ()
   (use-package default-text-scale
@@ -57,18 +60,14 @@
 (defun peter-misc/init-electric-operator ()
   (use-package electric-operator
     :defer t
-    :init
-    (progn
-    (add-hook 'python-mode-hook #'electric-operator-mode)
-    (add-hook 'c-mode-common-hook #'electric-operator-mode)
-    ;; ignore *, & operator in c/c++ mode
-    (add-hook 'electric-operator-mode-hook (lambda ()
-                                             (electric-operator-add-rules-for-mode 'c++-mode
-                                                                                   (cons "*" nil)
-                                                                                   (cons "&" nil))
-
-                                             (electric-operator-add-rules-for-mode 'c-mode
-                                                                                   (cons "*" nil)))))))
+    :hook ((c-mode-common . electric-operator-mode)
+           (python-mode . electric-operator-mode)
+           (electric-operator-mode . (lambda ()
+                                      (electric-operator-add-rules-for-mode 'c++-mode
+                                                                            (cons "*" nil)
+                                                                            (cons "&" nil))
+                                      (electric-operator-add-rules-for-mode 'c-mode
+                                                                            (cons "*" nil)))))))
 
 (defun peter-misc/init-evil-vimish-fold ()
   (use-package evil-vimish-fold
@@ -84,17 +83,18 @@
   (use-package highlight-indent-guides
     :defer t
     :hook ((prog-mode . highlight-indent-guides-mode))
+    (highlight-indent-guides-mode . (lambda ()
+                                      (set-face-foreground 'highlight-indent-guides-character-face "#8f9091")
+                                      (set-face-foreground 'highlight-indent-guides-top-character-face "#fe5e10")))
     :config
     (progn
-      (setq highlight-indent-guides-method 'character)
+      (setq highlight-indent-guides-method 'character
       ;; , ⋮, ┆, ┊, ┋, ┇
-      (setq highlight-indent-guides-character ?\┋)
-      (setq highlight-indent-guides-responsive 'top)
-      (setq highlight-indent-guides-auto-enabled nil)
-      (set-face-foreground 'highlight-indent-guides-character-face "#8f9091")
-      (set-face-foreground 'highlight-indent-guides-top-character-face "#fe5e10")
-      (setq highlight-indent-guides-auto-character-face-perc 10)
-      (setq highlight-indent-guides-auto-top-character-face-perc 20))))
+       highlight-indent-guides-character ?\┋
+       highlight-indent-guides-responsive 'top
+       highlight-indent-guides-auto-enabled nil
+       highlight-indent-guides-auto-character-face-perc 10
+       highlight-indent-guides-auto-top-character-face-perc 20))))
 
 (defun peter-misc/init-prettify-greek ()
   (use-package prettify-greek
@@ -104,5 +104,11 @@
   (use-package smart-semicolon
     :defer t
     :hook ((c-mode-common . smart-semicolon-mode))))
+
+(defun peter-misc/init-treemacs-icons-dired ()
+  (use-package treemacs-icons-dired
+    :if (display-graphic-p)
+    :defer t
+    :hook ((dired-mode . treemacs-icons-dired-mode))))
 
 ;;; packages.el ends here
