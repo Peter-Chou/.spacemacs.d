e53876fb88dad7e79ea26961f4c0fb6f48ecb263
Peter-Chou <zhou_00009@126.com>
2019-01-15 08:37:06 +0800

    Add: add config.el in peter-misc layer

---
+++ layers/peter-misc/config.el
@@ -0,0 +1,16 @@
+;;; packages.el --- peter-misc layer configuration file for Spacemacs.
+;;
+;; Copyright (c) 2018-2019 Peter Chou
+;;
+;; Author:  <Peter Chou>
+;; URL: https://github.com/Peter-Chou/spacemacs-settings
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; variables
+
+
+(defvar enable-electric-operator nil
+  "if not nil, enable electric operator mode")
