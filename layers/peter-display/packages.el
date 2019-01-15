cd2f872ad981c86a7a6b6efdffe3fa96302fb238
Peter-Chou <zhou_00009@126.com>
2019-01-15 12:14:49 +0800

    replace pretty-shell with eshell-git-prompt

--- layers/peter-display/packages.el
+++ layers/peter-display/packages.el
@@ -1,3 +1,4 @@
+;;; Display Layer -*- lexical-binding: t; -*-
 ;;; packages.el --- peter-display layer packages file for Spacemacs.
 ;;
 ;; Copyright (c) 2018-2019 Peter Chou
@@ -17,13 +18,13 @@
         all-the-icons-ivy
         all-the-icons-dired
         company-box
+        eshell-git-prompt
         (prettify-utils :location (recipe :fetcher github
                                           :repo "Ilazki/prettify-utils.el"))
         treemacs-icons-dired
         ;; Elsehwere-owned packages
         which-key
 
-        (pretty-eshell   :location local)
         (pretty-fonts    :location local)
         (pretty-magit    :location local)
         ))
@@ -60,6 +61,16 @@
     :defer t
     :hook (company-mode . company-box-mode)))
 
+
+(defun peter-display/init-eshell-git-prompt ()
+  (use-package eshell-git-prompt
+    :ensure t
+    :config
+    (with-eval-after-load 'eshell
+      (eshell-git-prompt-use-theme 'powerline))
+    ))
+
+
 ;;;; Prettify-utils
 
 (defun peter-display/init-prettify-utils ()
@@ -72,56 +83,6 @@
   (when (and (configuration-layer/package-used-p 'pretty-fonts) enable-font-ligature)
     (setq which-key-separator " ")
     (setq which-key-prefix-prefix " ")))
-
-(defun peter-display/init-pretty-eshell ()
-  (use-package pretty-eshell
-    :init
-    (progn
-      ;; Change default banner message
-      (setq eshell-banner-message (s-concat (s-repeat 20 "---") "\n\n"))
-
-      ;; More prompt styling
-      (setq pretty-eshell-header "\n︳")
-      (setq pretty-eshell-prompt-string " "))
-
-    :config
-    (progn
-      ;; Directory
-      (pretty-eshell-section
-       esh-dir
-       "\xf07c"  ; 
-       (abbreviate-file-name (eshell/pwd))
-       '(:foreground "#268bd2" :bold bold :underline t))
-
-      ;; Git Branch
-      (pretty-eshell-section
-       esh-git
-       "\xe907"  ; 
-       (magit-get-current-branch)
-       '(:foreground "#8D6B94"))
-
-      ;; Python Virtual Environment
-      (pretty-eshell-section
-       esh-python
-       "\xe928"  ; 
-       pyvenv-virtual-env-name)
-
-      ;; Time
-      (pretty-eshell-section
-       esh-clock
-       "\xf017"  ; 
-       (format-time-string "%H:%M" (current-time))
-       '(:foreground "forest green"))
-
-      ;; Prompt Number
-      (pretty-eshell-section
-       esh-num
-       "\xf0c9"  ; 
-       (number-to-string pretty-eshell-prompt-num)
-       '(:foreground "brown"))
-
-      (setq pretty-eshell-funcs
-            (list esh-dir esh-git esh-python esh-clock esh-num)))))
 
 ;;;; Pretty-fonts
 
