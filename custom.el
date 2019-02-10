87cbac5c4268a98888112dba631a8b776459e251
Peter-Chou <zhou_00009@126.com>
2019-02-10 13:12:05 +0800

    :lipstick:

--- custom.el
+++ custom.el
@@ -14,6 +14,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(epe-pipeline-time-face ((t (:foreground "#e2c504"))))
+ '(epe-pipeline-user-face ((t (:foreground "#ef2d2d" :weight bold))))
+ '(epe-pipeline-host-face ((t (:foreground "#3cd8a2" :weight bold))))
+ '(epe-pipeline-delimiter-face ((t (:foreground "#fd780f" :weight bold))))
  )
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
